c6c85bb3d169607f3220fe99f201f2075a19df29
Person0z <80363995+Person0z@users.noreply.github.com>
2025-01-14 01:19:15 -0600

    Update store.blade.php

--- resources/views/admin/jexactyl/store.blade.php
+++ resources/views/admin/jexactyl/store.blade.php
@@ -108,21 +108,21 @@
                                 <label class="control-label">Cost per 10% CPU</label>
                                 <div>
                                     <input type="text" class="form-control" name="store:cost:cpu" value="{{ $cpu }}" />
-                                    <p class="text-muted"><small>Used to calculate the total cost for 50% CPU.</small></p>
+                                    <p class="text-muted"><small>Used to calculate the total cost for 10% CPU.</small></p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <label class="control-label">Cost per 256MB RAM</label>
                                 <div>
                                     <input type="text" class="form-control" name="store:cost:memory" value="{{ $memory }}" />
-                                    <p class="text-muted"><small>Used to calculate the total cost for 1GB of RAM.</small></p>
+                                    <p class="text-muted"><small>Used to calculate the total cost for 256MB of RAM.</small></p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <label class="control-label">Cost per 512MB Disk</label>
                                 <div>
                                     <input type="text" class="form-control" name="store:cost:disk" value="{{ $disk }}" />
-                                    <p class="text-muted"><small>Used to calculate the total cost for 1GB of disk.</small></p>
+                                    <p class="text-muted"><small>Used to calculate the total cost for 512MB of disk.</small></p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
