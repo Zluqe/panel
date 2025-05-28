d1f7c6800832f7d9c7c13717ad9231dd0685182b
Person0z <80363995+Person0z@users.noreply.github.com>
2025-05-28 16:55:56 -0500

    Create DeleteSubuserRequest.php

---
+++ app/Http/Requests/Api/Application/Servers/Subuser/DeleteSubuserRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jexactyl\Http\Requests\Api\Application\Servers\Subuser;
+
+use Jexactyl\Http\Requests\Api\Application\ApplicationApiRequest;
+
+class DeleteSubuserRequest extends ApplicationApiRequest
+{
+    // Basic authorization handled by parent
+}
